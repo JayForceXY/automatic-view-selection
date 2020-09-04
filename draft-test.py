93650696d4d3be09e99ff2cf8426710464efd14d
Ahmed Derkaoui <a.derkaoui@esi-sba.dz>
2020-09-04 20:48:16 +0100

    draft-test.py is now modular & query_mapping.py has function for SQL to Algebric transformation

--- draft-test.py
+++ draft-test.py
@@ -1,7 +1,24 @@
 from pyhive import  presto
 import requests
 import numpy as np
+import sqlite3
+cursor = presto.connect ( host='localhost',
+                        catalog='hive',
+                        schema='tpch').cursor ( )
 
-cursor = presto.connect ( 'localhost' ).cursor ( )
-cursor.execute ( 'SELECT count(*) from tpch.customer' )
-print ( cursor.fetchone ( ) );
+# Open and read the file as a single buffer
+fd = open('batch_queries.sql', 'r')
+sqlFile = fd.read()
+fd.close()
+
+# all SQL commands (split on ';')
+commands = sqlFile.split(';')
+query_set = []
+for command in commands:
+    query_set.append(command)
+
+
+
+print(query_set[0])
+cursor.execute("EXPLAIN select customer.nation, supplier.nation, date_dim.year, sum(revenue) as revenue from lineorder left join date_dim on lineorder.orderdate = date_dim.datekey left join customer on lineorder.custkey = customer.custkey left join supplier on lineorder.suppkey = supplier.suppkey where customer.region = 'ASIA' and supplier.region = 'ASIA' and date_dim.year >= 1992 and date_dim.year <= 1997 group by customer.nation, supplier.nation,date_dim.year order by date_dim.year asc,revenue desc")
+print(cursor.fetchall())
